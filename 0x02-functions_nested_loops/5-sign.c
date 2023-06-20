f18b0ebfcb304cd2a20ce0a3e5ef99d461209f16
othmanekoddam <othmanekoddam@outlook.fr>
2023-06-20 12:38:07 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,12 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_sign - Determines whether an integer is positive, negative or zero.
- * @n: the number to be checked.
- * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
@@ -16,14 +20,9 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
 	else
 	{
 		_putchar('-');
-		return ('/');
+		return (-1);
 	}
 }
